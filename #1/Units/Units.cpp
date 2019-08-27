cc4f01f6618c6676ddd076299f1c292f6909fb98
Pavel Vlasanek <pavel.vlasanek@tucna.net>
2019-08-27 18:02:47 +0200

    Extension - literals

--- #1/Units/Units.cpp
+++ #1/Units/Units.cpp
@@ -8,9 +8,13 @@
 int main()
 {
   Test test;
-  Momentum momentum{ 13.75 };
+
+  constexpr Speed speed = Length{ 2.0 } / Time{ 5.0 };
+  constexpr Momentum momentum = Force{ 30.0 } *Time{ 3.0 };
+
+  Force force = 30.0_N;
+  Time time = 3.0_s;
+  Momentum momentumDifferent = force * time;
 
   test.applyMomentum(momentum);
-
-  std::cout << "Hello World!\n";
 }
--- #1/Units/Value.h
+++ #1/Units/Value.h
@@ -18,6 +18,30 @@ private:
   long double _magnitude{ 0.0 };
 };
 
+template <int M, int K, int S>
+constexpr Value<MksUnit<M, K, S>> operator+ (const Value<MksUnit<M, K, S>>& lhs, const Value<MksUnit<M, K, S>>& rhs) noexcept
+{
+  return Value<MksUnit<M, K, S>>(lhs.getMagnitude() + rhs.getMagnitude());
+}
+
+template <int M, int K, int S>
+constexpr Value<MksUnit<M, K, S>> operator- (const Value<MksUnit<M, K, S>>& lhs, const Value<MksUnit<M, K, S>>& rhs) noexcept
+{
+  return Value<MksUnit<M, K, S>>(lhs.getMagnitude() - rhs.getMagnitude());
+}
+
+template <int M1, int K1, int S1, int M2, int K2, int S2>
+constexpr Value<MksUnit<M1 + M2, K1 + K2, S1 + S2>> operator* (const Value<MksUnit<M1, K1, S1>>& lhs, const Value<MksUnit<M2, K2, S2>>& rhs) noexcept
+{
+  return Value<MksUnit<M1 + M2, K1 + K2, S1 + S2>>(lhs.getMagnitude() * rhs.getMagnitude());
+}
+
+template <int M1, int K1, int S1, int M2, int K2, int S2>
+constexpr Value<MksUnit<M1 - M2, K1 - K2, S1 - S2>> operator/ (const Value<MksUnit<M1, K1, S1>>& lhs, const Value<MksUnit<M2, K2, S2>>& rhs) noexcept
+{
+  return Value<MksUnit<M1 - M2, K1 - K2, S1 - S2>>(lhs.getMagnitude() / rhs.getMagnitude());
+}
+
 using DimensionlessQuantity = Value<MksUnit<0, 0, 0>>;
 using Length = Value<MksUnit<1, 0, 0>>;
 using Area = Value<MksUnit<2, 0, 0>>;
@@ -30,6 +54,26 @@ using Frequency = Value<MksUnit<0, 0, -1>>;
 using Force = Value<MksUnit<1, 1, -2>>;
 using Pressure = Value<MksUnit<-1, 1, -2>>;
 using Momentum = Value<MksUnit<1, 1, -1>>;
+
+constexpr Force operator"" _N(long double magnitude)
+{
+  return Force(magnitude);
+}
+
+constexpr Acceleration operator"" _ms2(long double magnitude)
+{
+  return Acceleration(magnitude);
+}
+
+constexpr Time operator"" _s(long double magnitude)
+{
+  return Time(magnitude);
+}
+
+constexpr Momentum operator"" _Ns(long double magnitude)
+{
+  return Momentum(magnitude);
+}
 
 constexpr Acceleration gravitationalAccelerationOnEarth{ 9.80665 };
 constexpr Pressure standardPressureOnSeaLevel{ 1013.25 };
